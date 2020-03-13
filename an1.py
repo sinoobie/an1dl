7b9d6c3d6409153b2c8a4a89bcd6d8f84774fac6
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2020-03-13 21:32:18 +0700

    #Update UI downloading proccess

--- an1.py
+++ an1.py
@@ -1,14 +1,17 @@
-import requests,re,sys,click,os
+import requests,re,sys,click,os,time
 from bs4 import BeautifulSoup as Bs
 
-info={}
+ses=requests.Session()
+info={
+	'title':[],
+	'dl':[],
+}
 
 def search(query):
 	c=1
-	req=requests.get(f'https://an1.com/tags/MOD/?story={query}&do=search&subaction=search')
+	req=ses.get(f'https://an1.com/tags/MOD/?story={query}&do=search&subaction=search')
 	bs=Bs(req.text,'html.parser')
 	tit=bs.find_all('div',{'class':'title'})
-	info['title']=[]
 	for x in tit:
 		try:
 			info['title'].append((f'{c}. {x.text}',x.find('a').get('href')))
@@ -18,7 +21,6 @@ def search(query):
 
 def grep():
 	cc=1
-	info['dl']=[]
 	for i in info['title']:
 		print(i[0])
 	pil=int(input('pilih_> '))
@@ -27,7 +29,7 @@ def grep():
 		return True
 	get=info['title'][pil-1][1]
 	print()
-	req2=requests.get(get)
+	req2=ses.get(get)
 	bs2=Bs(req2.text,'html.parser')
 	prd=bs2.find_all('div',{'class':'get-product-holder'})
 	for y in prd:
@@ -52,11 +54,11 @@ def grep():
 
 def download(url,judul):
 	print("#",re.findall(r'. (.*)',judul)[0])
-	req3=requests.get('https://an1.com'+url)
+	req3=ses.get('https://an1.com'+url)
 	rg=re.findall(r'href=(.*)><input',req3.text)
 	
 	if "bit.ly" in rg[0]:
-		rq=requests.get(rg[0]).url
+		rq=ses.get(rg[0]).url
 		rg=rq.split()
 	
 	if "files.an1.net" in rg[0]:
@@ -64,11 +66,10 @@ def download(url,judul):
 	elif '.apk' in rg[0] or '.zip' in rg[0]:
 		link=rg[0]
 	elif "www.4sync.com" in rg[0]:
-		req4=requests.get(rg[0])
+		req4=ses.get(rg[0])
 		bs3=Bs(req4.text,'html.parser')
 		link=bs3.find('input',{'class':'jsDLink'})['value']
 	elif "racaty.com" in rg[0]:
-		ses=requests.Session()
 		reqs=ses.get(rg[0])
 		bss=Bs(reqs.text,'html.parser')
 		op=bss.find('input',{'name':'op'})['value']
@@ -78,17 +79,20 @@ def download(url,judul):
 		bss2=Bs(rep.text,'html.parser')
 		link=bss2.find('div',{'id':'DIV_1'}).find('a')['href']
 	else:
-		bps=requests.get(rg[0]).url
+		bps=ses.get(rg[0]).url
 		nya=input(f"[Maaf] link download {re.findall(r'https://(.*)/',bps)} saat ini belum kami support\n[?] Apakah anda ingin membuka link tersebut (y/n) ")
 		if nya.lower() == 'y':
 			click.launch(rg[0])
 		return True
+#	print(info)
 	print(f"{rg}\n")
 
 	#Downloading
 	file=re.findall(r'Download (.*)  ',judul)[0]
 	with open(f'result/{file.replace("/",",")}','wb') as save:
+		count=1
 		response=requests.get(link,stream=True)
+		start=time.time()
 		total_length=response.headers.get('content-length')
 		if total_length is None:
 			print("\n[Warn] Download GAGAL")
@@ -101,11 +105,18 @@ def download(url,judul):
 			dlw=0
 			total_length=int(total_length)
 			for data in response.iter_content(chunk_size=4096):
+				durasi=time.time() - start
+				if durasi == 0:
+					durasi=0.1
+				
 				ges=int(100*dlw/total_length)
+				dsiz=int(count*4096)
+				sped=int(dsiz/(1024*durasi))
 				dlw+=len(data)
 				save.write(data)
-				done=int(30*dlw/total_length)
-				print(end=f"\r\033[97m[\033[92m{'>'*done}\033[91m{'='*(30-done)}\033[97m] \033[96m{ges+1}% ",flush=True)
+				done=int(15*dlw/total_length)
+				print(end=f"\r\033[97m[\033[92m{'>'*done}\033[91m{'='*(15-done)}\033[97m] {ges+1}%, {round(dsiz/(1024*1024), 2)} MB, {sped} KB/s  ",flush=True)
+				count+=1
 	print("\n[OK] file saved in result")
 
 if __name__ == "__main__":
